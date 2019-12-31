1909111f7b56e1bfca4689b1341474fd416b512f
00757033 <58459235+00757033@users.noreply.github.com>
2019-12-31 13:50:49 +0800

    12/31

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
-
     var story="Pride and Prejudice";
     $("#start").html('<h1 class="example" data-in-effect="flipInX">'+story+'</h1>');
     $( ".example" ).textillate();
-    setTimeout(first, 1000);
 });
 
 
